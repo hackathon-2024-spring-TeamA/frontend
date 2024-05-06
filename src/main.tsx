ecb13ff6f096a2789ceb449ab50d426d4a8ce29a
hiroki <hiroki.hkt@gmail.com>
2024-05-07 07:13:29 +0900

    Amplify&Cognito連携のサンプル

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import React from "react";
 
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
+import { Amplify } from "aws-amplify";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
+import awsExports from "./aws-exports";
 import router from "./router.tsx";
 import theme from "./theme.ts";
+
+Amplify.configure(awsExports);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,8 @@
 import {
+  withAuthenticator,
+  WithAuthenticatorProps,
+} from "@aws-amplify/ui-react";
+import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
@@ -10,17 +14,69 @@ import MainLayout from "@/components/Layout/MainLayout";
 import MockAboutPage from "@/pages/mock/MockAboutPage";
 import MockHomePage from "@/pages/mock/MockHomePage";
 
+import "@aws-amplify/ui-react/styles.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import React from "react";
+
+import { Button } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#ffffff",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+  },
+});
+
+const AuthWrapper: React.FC<WithAuthenticatorProps> = ({ signOut, user }) => (
+  <ThemeProvider theme={theme}>
+    <div>
+      {user ? (
+        <>
+          <p>Welcome, {user.username}</p>
+          <Button variant="contained" color="secondary" onClick={signOut}>
+            Sign Out
+          </Button>
+        </>
+      ) : (
+        <>
+          <Button variant="contained" color="primary" href="/login">
+            Sign In
+          </Button>
+          <Button variant="contained" color="secondary" href="/signup">
+            Sign Up
+          </Button>
+        </>
+      )}
+    </div>
+  </ThemeProvider>
+);
+
+const AuthenticatedHomePage = withAuthenticator(MockHomePage);
+const AuthenticatedAuthWrapper = withAuthenticator(AuthWrapper);
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<MainLayout />}>
-      <Route path="/">{/* 作成した本番用Pageコンポーネントを配置 */}</Route>
+      <Route
+        path="/"
+        element={
+          <AuthenticatedAuthWrapper>
+            <AuthenticatedHomePage />
+          </AuthenticatedAuthWrapper>
+        }
+      />
       <Route path="/mock">
         {/* // mock用ページ */}
         <Route index element={<MockHomePage />} />
         <Route path="about" element={<MockAboutPage />} />
         <Route path="cards" element={<MockCards />} />
       </Route>
-      ,
     </Route>,
   ),
 );
