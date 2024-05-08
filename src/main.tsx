ca98d7fc22049cc7a380b718d271dce756af20dd
hosomatu <kcfdmori0759@gmail.com>
2024-05-09 01:54:05 +0900

    最低限のログイン機能

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import React from "react";
 import { ApolloProvider } from "@apollo/client";
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
+import { Amplify } from "aws-amplify";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
 import { client } from "./apollo/client.ts";
+import awsExports from "./aws-exports";
 import router from "./router.tsx";
 import theme from "./theme.ts";
+
+Amplify.configure(awsExports);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,8 @@
 import {
+  withAuthenticator,
+  WithAuthenticatorProps,
+} from "@aws-amplify/ui-react";
+import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
@@ -15,10 +19,68 @@ import MockAboutPage from "@/pages/mock/MockAboutPage";
 import MockGqlPage from "@/pages/mock/MockGqlPage";
 import MockHomePage from "@/pages/mock/MockHomePage";
 
+import "@aws-amplify/ui-react/styles.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Button } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#ffffff",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+  },
+});
+
+const AuthWrapper: React.FC<WithAuthenticatorProps> = ({ signOut, user }) => (
+  <ThemeProvider theme={theme}>
+    <div>
+      {user ? (
+        <>
+          <p>Welcome, {user.username}</p>
+          <Button variant="contained" color="secondary" onClick={signOut}>
+            Sign Out
+          </Button>
+        </>
+      ) : (
+        <>
+          <Button variant="contained" color="primary" href="/login">
+            Sign In
+          </Button>
+          <Button variant="contained" color="secondary" href="/signup">
+            Sign Up
+          </Button>
+        </>
+      )}
+    </div>
+  </ThemeProvider>
+);
+
+// PropTypes の定義
+AuthWrapper.propTypes = {
+  signOut: PropTypes.func.isRequired,
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+const AuthenticatedHomePage = withAuthenticator(MockHomePage);
+const AuthenticatedAuthWrapper = withAuthenticator(AuthWrapper);
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<MainLayout />}>
-      <Route path="/">
+      <Route
+        path="/"
+        element={
+          <AuthenticatedAuthWrapper>
+            <AuthenticatedHomePage />
+          </AuthenticatedAuthWrapper>
+        }
+      >
         {/* 作成した本番用Pageコンポーネントを配置 */}
         <Route path="donation">
           {/* 寄付機能 PATH */}
