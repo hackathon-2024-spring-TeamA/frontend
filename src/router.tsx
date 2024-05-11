42d81e0bbf0636225305e26aa411be27718c1207
hiroki <hiroki.hkt@gmail.com>
2024-05-11 16:33:43 +0900

    [mod]amplifyデプロイのための修正（テスト）

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 import {
   withAuthenticator,
-  WithAuthenticatorProps,
+  // WithAuthenticatorProps,
 } from "@aws-amplify/ui-react";
 import {
   createBrowserRouter,
@@ -20,68 +20,38 @@ import MockGqlPage from "@/pages/mock/MockGqlPage";
 import MockHomePage from "@/pages/mock/MockHomePage";
 
 import "@aws-amplify/ui-react/styles.css";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { Button } from "@mui/material";
-import PropTypes from "prop-types";
 
-const theme = createTheme({
-  palette: {
-    mode: "dark",
-    primary: {
-      main: "#ffffff",
-    },
-    secondary: {
-      main: "#f50057",
-    },
-  },
-});
-
-const AuthWrapper: React.FC<WithAuthenticatorProps> = ({ signOut, user }) => (
-  <ThemeProvider theme={theme}>
-    <div>
-      {user ? (
-        <>
-          <p>Welcome, {user.username}</p>
-          <Button variant="contained" color="secondary" onClick={signOut}>
-            Sign Out
-          </Button>
-        </>
-      ) : (
-        <>
-          <Button variant="contained" color="primary" href="/login">
-            Sign In
-          </Button>
-          <Button variant="contained" color="secondary" href="/signup">
-            Sign Up
-          </Button>
-        </>
-      )}
-    </div>
-  </ThemeProvider>
-);
-
-// PropTypes の定義
-AuthWrapper.propTypes = {
-  signOut: PropTypes.func.isRequired,
-  user: PropTypes.shape({
-    username: PropTypes.string.isRequired,
-  }).isRequired,
-};
+// const AuthWrapper: React.FC<WithAuthenticatorProps> = ({ signOut, user }) => (
+//   <ThemeProvider theme={theme}>
+//     <div>
+//       {user ? (
+//         <>
+//           <p>Welcome, {user.username}</p>
+//           <Button variant="contained" color="secondary" onClick={signOut}>
+//             Sign Out
+//           </Button>
+//         </>
+//       ) : (
+//         <>
+//           <Button variant="contained" color="primary" href="/login">
+//             Sign In
+//           </Button>
+//           <Button variant="contained" color="secondary" href="/signup">
+//             Sign Up
+//           </Button>
+//         </>
+//       )}
+//     </div>
+//   </ThemeProvider>
+// );
 
 const AuthenticatedHomePage = withAuthenticator(MockHomePage);
-const AuthenticatedAuthWrapper = withAuthenticator(AuthWrapper);
+// const AuthenticatedAuthWrapper = withAuthenticator(AuthWrapper);
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<MainLayout />}>
-      <Route
-        path="/"
-        element={
-          <AuthenticatedAuthWrapper>
-            <AuthenticatedHomePage />
-          </AuthenticatedAuthWrapper>
-        }
-      >
+      <Route path="/" element={<AuthenticatedHomePage />}>
         {/* 作成した本番用Pageコンポーネントを配置 */}
         <Route path="donation">
           {/* 寄付機能 PATH */}
