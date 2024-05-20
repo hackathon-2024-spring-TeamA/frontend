70ad93ca6106aadbdc47fc84d4c80b79724b7e66
hana222444 <hanar6stake@gmail.com>
2024-05-20 22:57:47 +0900

    ログイン後ヘッダー追加

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,6 +5,11 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Toolbar from "@mui/material/Toolbar";
 
+import User_option from "/src/assets/images/user_option.png";
+import Book_management from "/src/assets/images/book_management.png";
+import Book_donation from "/src/assets/images/book_donation.png";
+import Logout from "/src/assets/images/logout.png";
+
 const Header: React.FC = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -14,8 +19,34 @@ const Header: React.FC = () => {
             <Button color="inherit">tech-libra</Button>
           </Box>
           <Box>
-            <Button color="inherit">サービス概要</Button>
-            <Button color="inherit">ログイン</Button>
+            <Button color="inherit" sx={{ mr: 1.3 }}>
+              <img
+                src={User_option}
+                style={{ width: "25px", height: "25px", marginRight: "10px" }}
+              />
+              ユーザー設定
+            </Button>
+            <Button color="inherit" sx={{ mx: 2 }}>
+              <img
+                src={Book_management}
+                style={{ width: "25px", height: "25px", marginRight: "10px" }}
+              />
+              本の貸借管理
+            </Button>
+            <Button color="inherit" sx={{ ml: 1 }}>
+              <img
+                src={Book_donation}
+                style={{ width: "25px", height: "25px", marginRight: "10px" }}
+              />
+              本の寄付
+            </Button>
+            <Button color="inherit" sx={{ ml: 0 }}>
+              <img
+                src={Logout}
+                style={{ width: "25px", height: "25px", marginRight: "10px" }}
+              />
+              ログアウト
+            </Button>
           </Box>
         </Toolbar>
       </AppBar>
--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,30 +1,46 @@
 import React from "react";
 
 const AboutPage: React.FC = () => {
+  const containerStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "70vh",
+  };
+
+  const boxStyle = {
+    padding: "20px",
+    border: "2px solid #333",
+    borderRadius: "10px",
+    backgroundColor: "white",
+  };
+
   return (
-    <div>
-      <h1>サービス概要</h1>
-      <div>
-        <p>
-          tech-libraは、技術書の相互貸借を可能にするプラットフォームです。このアプリを通じて、以下の課題を解決します。
-        </p>
-        <ol>
-          <li>
-            技術書を読みたいが購入できないユーザーへの支援
-            <br />
-            ユーザー同士で技術書を無料で貸し借りできます。希望の書籍があれば、貸出可能なユーザーから借りることができます。
-          </li>
-          <li>
-            不要になった技術書の有効活用
-            <br />
-            読み終わった技術書の置き場所に困っているユーザーは、アプリ上であの寄付を行うことで他のユーザーに提供できます。
-          </li>
-          <li>
-            RareTECHコミュニティへの貢献
-            <br />
-            不要な技術書を提供することで、コミュニティ内での知識共有と助け合いに貢献できます。
-          </li>
-        </ol>
+    <div style={containerStyle}>
+      <div style={boxStyle}>
+        <h1>サービス概要</h1>
+        <div>
+          <p>
+            tech-libraは、技術書の相互貸借を可能にするプラットフォームです。このアプリを通じて、以下の課題を解決します。
+          </p>
+          <ol>
+            <li>
+              技術書を読みたいが購入できないユーザーへの支援
+              <br />
+              ユーザー同士で技術書を無料で貸し借りできます。希望の書籍があれば、貸出可能なユーザーから借りることができます。
+            </li>
+            <li>
+              不要になった技術書の有効活用
+              <br />
+              読み終わった技術書の置き場所に困っているユーザーは、アプリ上であの寄付を行うことで他のユーザーに提供できます。
+            </li>
+            <li>
+              RareTECHコミュニティへの貢献
+              <br />
+              不要な技術書を提供することで、コミュニティ内での知識共有と助け合いに貢献できます。
+            </li>
+          </ol>
+        </div>
       </div>
     </div>
   );
