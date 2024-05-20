5494d27017172eb847a2b3e4c08f41f968e6494f
hiroki <hiroki.hkt@gmail.com>
2024-05-20 20:09:39 +0900

    [add]一覧、検索機能で使うGraphQLのクエリスキーマを追加する

---
+++ src/features/search/queries.ts
@@ -0,0 +1,34 @@
+import { gql } from "@apollo/client";
+
+export const SEARCH_BOOKS = gql`
+  query SearchBooks($page: Int!, $perPage: Int!, $searchQuery: String) {
+    searchBooks(page: $page, perPage: $perPage, searchQuery: $searchQuery) {
+      totalCount
+      currentPage
+      perPage
+      books {
+        id
+        user_id
+        donation_date
+        book_information {
+          book_information_id
+          isbn_number
+          title
+          author
+          published_date
+          description
+          image_path
+        }
+        latest_book_loan {
+          id
+          user_id
+          book_id
+          rent_date
+          due_date
+          return_date
+          is_held
+        }
+      }
+    }
+  }
+`;
