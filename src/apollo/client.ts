82ad2f7ab8ae3d3bd5e74f96b5a96c48cb3b1eff
hiroki <hiroki.hkt@gmail.com>
2024-05-23 01:33:57 +0900

    gqlのuriをALBのものに変更する

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:8888/graphql",
+  uri: "http://newcdk-albae-ihrfbrmsurpq-1854546335.ap-northeast-1.elb.amazonaws.com/graphql",
 });
 
 export const client = new ApolloClient({
