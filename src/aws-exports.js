16a794405158337a4189bba46e4de548a0daa60f
hiroki <hiroki.hkt@gmail.com>
2024-05-22 22:52:29 +0900

    amplify addによってaws-exports.jsが更新された状態

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,6 +3,25 @@
 
 const awsmobile = {
   aws_project_region: "ap-northeast-1",
+  aws_cognito_region: "ap-northeast-1",
+  aws_user_pools_id: "ap-northeast-1_m0MDerwnY",
+  aws_user_pools_web_client_id: "7a3rbh05qb0c9f9631mvf8s6gh",
+  oauth: {},
+  aws_cognito_username_attributes: ["EMAIL"],
+  aws_cognito_social_providers: [],
+  aws_cognito_signup_attributes: ["EMAIL", "NICKNAME"],
+  aws_cognito_mfa_configuration: "OFF",
+  aws_cognito_mfa_types: [],
+  aws_cognito_password_protection_settings: {
+    passwordPolicyMinLength: 8,
+    passwordPolicyCharacters: [
+      "REQUIRES_LOWERCASE",
+      "REQUIRES_UPPERCASE",
+      "REQUIRES_NUMBERS",
+      "REQUIRES_SYMBOLS",
+    ],
+  },
+  aws_cognito_verification_mechanisms: ["EMAIL"],
 };
 
 export default awsmobile;
