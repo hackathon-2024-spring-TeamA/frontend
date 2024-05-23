fd1f2d81710cfc0a158cba33c783a77b88fc0c4e
hosomatu <kcfdmori0759@gmail.com>
2024-05-24 00:01:30 +0900

    client.tsにバックエンドのURIを追加

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
 const httpLink = createHttpLink({
-  uri: "http://newcdk-albae-ihrfbrmsurpq-1854546335.ap-northeast-1.elb.amazonaws.com/graphql",
+  uri: "https://api.tech-libra.com/graphql",
 });
 
 export const client = new ApolloClient({
