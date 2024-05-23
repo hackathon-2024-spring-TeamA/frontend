8991d1260f3755525aa5c03807a96b485202c464
hana222444 <hanar6stake@gmail.com>
2024-05-23 21:22:38 +0900

    未ログイン状態でのヘッダー（完成）

--- src/components/Header/NotLoginHeader.tsx
+++ src/components/Header/NotLoginHeader.tsx
@@ -5,13 +5,24 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Toolbar from "@mui/material/Toolbar";
 
+import Tech_libra_header from "/src/assets/images/tech_libra_header.png";
+
 const NotLoginHeader: React.FC = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="sticky">
         <Toolbar>
           <Box sx={{ flexGrow: 1 }}>
-            <Button color="inherit">tech-libra</Button>
+            <Button color="inherit">
+              <img
+                src={Tech_libra_header}
+                style={{
+                  width: "206.05px",
+                  height: "50px",
+                  marginRight: "10px",
+                }}
+              />
+            </Button>
           </Box>
           <Box>
             <Button color="inherit">サービス概要</Button>
