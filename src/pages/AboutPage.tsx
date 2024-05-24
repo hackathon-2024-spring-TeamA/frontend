833779883c461a53da7a86cb192c9bdc09f4bd2b
hana222444 <hanar6stake@gmail.com>
2024-05-24 19:33:08 +0900

    サービス概要ページ調整

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -5,7 +5,7 @@ const AboutPage: React.FC = () => {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    height: "70vh",
+    height: "85vh",
   };
 
   const boxStyle = {
@@ -15,27 +15,31 @@ const AboutPage: React.FC = () => {
     backgroundColor: "white",
   };
 
+  const titleStyle = {
+    marginTop: "0",
+  };
+
   return (
     <div style={containerStyle}>
       <div style={boxStyle}>
-        <h1>サービス概要</h1>
+        <h1 style={titleStyle}>サービス概要</h1>
         <div>
           <p>
             tech-libraは、技術書の相互貸借を可能にするプラットフォームです。このアプリを通じて、以下の課題を解決します。
           </p>
           <ol>
             <li>
-              技術書を読みたいが購入できないユーザーへの支援
+              <strong>技術書を読みたいが購入できないユーザーへの支援</strong>
               <br />
               ユーザー同士で技術書を無料で貸し借りできます。希望の書籍があれば、貸出可能なユーザーから借りることができます。
             </li>
             <li>
-              不要になった技術書の有効活用
+              <strong>不要になった技術書の有効活用</strong>
               <br />
               読み終わった技術書の置き場所に困っているユーザーは、アプリ上であの寄付を行うことで他のユーザーに提供できます。
             </li>
             <li>
-              RareTECHコミュニティへの貢献
+              <strong>RareTECHコミュニティへの貢献</strong>
               <br />
               不要な技術書を提供することで、コミュニティ内での知識共有と助け合いに貢献できます。
             </li>
